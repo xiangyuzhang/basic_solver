2a106992fa844daaa60a88dec271bbd6a8a0b320
xiangyu <xiangyuzhang365@gmail.com>
2016-07-07 15:49:33 -0400

    init commit

---
+++ simp/depend.mk
@@ -0,0 +1,32 @@
+/Users/xiangyuzhang/Github/minisat/simp/Main.o /Users/xiangyuzhang/Github/minisat/simp/Main.or /Users/xiangyuzhang/Github/minisat/simp/Main.od /Users/xiangyuzhang/Github/minisat/simp/Main.op: /Users/xiangyuzhang/Github/minisat/simp/Main.cc \
+ /Users/xiangyuzhang/Github/minisat/utils/System.h \
+ /Users/xiangyuzhang/Github/minisat/mtl/IntTypes.h \
+ /Users/xiangyuzhang/Github/minisat/utils/ParseUtils.h \
+ /Users/xiangyuzhang/Github/minisat/utils/Options.h \
+ /Users/xiangyuzhang/Github/minisat/mtl/Vec.h \
+ /Users/xiangyuzhang/Github/minisat/mtl/XAlloc.h \
+ /Users/xiangyuzhang/Github/minisat/core/Dimacs.h \
+ /Users/xiangyuzhang/Github/minisat/core/SolverTypes.h \
+ /Users/xiangyuzhang/Github/minisat/mtl/Alg.h \
+ /Users/xiangyuzhang/Github/minisat/mtl/Map.h \
+ /Users/xiangyuzhang/Github/minisat/mtl/Alloc.h \
+ /Users/xiangyuzhang/Github/minisat/simp/SimpSolver.h \
+ /Users/xiangyuzhang/Github/minisat/mtl/Queue.h \
+ /Users/xiangyuzhang/Github/minisat/core/Solver.h \
+ /Users/xiangyuzhang/Github/minisat/mtl/Heap.h
+/Users/xiangyuzhang/Github/minisat/simp/SimpSolver.o /Users/xiangyuzhang/Github/minisat/simp/SimpSolver.or /Users/xiangyuzhang/Github/minisat/simp/SimpSolver.od /Users/xiangyuzhang/Github/minisat/simp/SimpSolver.op: /Users/xiangyuzhang/Github/minisat/simp/SimpSolver.cc \
+ /Users/xiangyuzhang/Github/minisat/mtl/Sort.h \
+ /Users/xiangyuzhang/Github/minisat/mtl/Vec.h \
+ /Users/xiangyuzhang/Github/minisat/mtl/IntTypes.h \
+ /Users/xiangyuzhang/Github/minisat/mtl/XAlloc.h \
+ /Users/xiangyuzhang/Github/minisat/simp/SimpSolver.h \
+ /Users/xiangyuzhang/Github/minisat/mtl/Queue.h \
+ /Users/xiangyuzhang/Github/minisat/core/Solver.h \
+ /Users/xiangyuzhang/Github/minisat/mtl/Heap.h \
+ /Users/xiangyuzhang/Github/minisat/mtl/Alg.h \
+ /Users/xiangyuzhang/Github/minisat/utils/Options.h \
+ /Users/xiangyuzhang/Github/minisat/utils/ParseUtils.h \
+ /Users/xiangyuzhang/Github/minisat/core/SolverTypes.h \
+ /Users/xiangyuzhang/Github/minisat/mtl/Map.h \
+ /Users/xiangyuzhang/Github/minisat/mtl/Alloc.h \
+ /Users/xiangyuzhang/Github/minisat/utils/System.h
